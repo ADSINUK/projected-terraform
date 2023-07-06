ced77417fd95c6f0b7d246681e081f1026ea74a7
Yurii Liaskovets <yurii.liaskovets@automat-it.com>
2023-07-07 01:31:40 +0300

    Create ACM certificate for DEV
    Signed-off-by: Yurii Liaskovets <yurii.liaskovets@automat-it.com>

---
+++ environments/dev/acm.tf
@@ -0,0 +1,20 @@
+resource "aws_acm_certificate" "dev" {
+  domain_name = "develop.${local.project_domain}"
+  subject_alternative_names = [
+    "develop-admin.${local.project_domain}",
+    "develop-api.${local.project_domain}",
+    "develop-app.${local.project_domain}",
+  ]
+  validation_method = "DNS"
+  lifecycle { create_before_destroy = true }
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn = aws_acm_certificate.dev.arn
+}
+
+output "acm" {
+  value = {
+    validation = aws_acm_certificate.dev.domain_validation_options
+  }
+}
